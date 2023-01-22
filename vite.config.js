7935303b821a320c90620cfa6b7addb60fec3a52
Nderi12 <nderikamau1212@gmail.com>
2023-01-23 00:23:42 +0300

    resolving css issue

--- vite.config.js
+++ vite.config.js
@@ -6,23 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/assets/css/nucleo-icons.css',
-                'resources/assets/css/blk-design-system.css?v=1.0.0',
-                'resources/assets/demo/demo.css',
-
                 'resources/js/app.js',
-                
-                'resources/assets/js/core/jquery.min.js',
-                'resources/assets/js/core/popper.min.js',
-                'resources/assets/js/core/bootstrap.min.js',
-                'resources/assets/js/plugins/perfect-scrollbar.jquery.min.js',
-                'resources/assets/js/plugins/bootstrap-switch.js',
-                'resources/assets/js/plugins/nouislider.min.js',
-                'resources/assets/js/plugins/chartjs.min.js',
-                'resources/assets/js/plugins/moment.min.js',
-                'resources/assets/js/plugins/bootstrap-datetimepicker.js',
-                'resources/assets/demo/demo.js',
-                'resources/assets/js/blk-design-system.min.js?v=1.0.0'
             ],
             refresh: true,
         }),
